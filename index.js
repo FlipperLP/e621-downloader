67ca06ea591ddee6b26915b63b860ae23c960523
Phil <philip.schaefer0@gmail.com>
2021-09-15 17:41:00 +0200

    added log and changed values

--- index.js
+++ index.js
@@ -51,14 +51,15 @@ function createEntry(id, md5, file_ext, tag_string, tag_count_general) {
 async function getPics(page) {
   const { body } = await endpoint('posts.json', {
     searchParams: {
-      tags: 'id:>=9000 order:id_asc -status:deleted', page, limit: 1000, login: 'Flipper', api_key: 'xTX3rSxE2A7tVUDDWaGYDWSB',
+      tags: 'id:>=100000 order:id_asc -status:deleted', page, limit: 320, login: 'Flipper', api_key: 'xTX3rSxE2A7tVUDDWaGYDWSB',
     },
   });
   return body.posts;
 }
 
 async function downloadFile(url, md5, ext) {
-  got.stream(url).pipe(createWriteStream(`${await createDir(md5)}/${md5}.${ext}`));
+  await got.stream(url).pipe(createWriteStream(`${await createDir(md5)}/${md5}.${ext}`));
+  console.log(`Prcessed ${md5}`);
 }
 
 //
@@ -74,19 +75,22 @@ async function downloadFile(url, md5, ext) {
   await console.log('[DB] Done syncing!');
 
   // cyceling 50 pages
-  for (let i = 0; i < 10; i++) {
+  for (let i = 4; i < 10; i++) {
     setTimeout(async () => {
       const posts = await getPics(i);
-      posts.forEach((post) => {
-        const tags = post.tags.general.join(' ');
-        const md5 = post.file.md5;
-        const ext = post.file.ext;
-        const url = post.sample.url || post.file.url;
-        if (url) {
-          createEntry(post.id, md5, ext, tags, tags.length);
-          downloadFile(url, md5, ext);
-        } else console.warn('No URL present, skipping.');
+      posts.forEach((post, entry) => {
+        setTimeout(() => {
+          console.log(`Starting ${i}.${entry}`);
+          const tags = post.tags.general.join(' ');
+          const md5 = post.file.md5;
+          const ext = post.file.ext;
+          const url = post.sample.url || post.file.url;
+          if (url) {
+            createEntry(post.id, md5, ext, tags, tags.length);
+            downloadFile(url, md5, ext);
+          } else console.warn('No URL present, skipping.');
+        }, 1000 * entry);
       });
-    }, 20000 * i);
+    }, 310000 * (i - 4));
   }
 })();
